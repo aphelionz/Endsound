76124f09f159ede2fda085047a6573c5f83f5496
Jesse Kochis <jessekochis@gmail.com>
2011-01-29 11:53:53 -0500

    Tone generator added.

--- server.js
+++ server.js
@@ -31,5 +31,24 @@ send404 = function(res){
 
 server.listen(666);
 
+// socket.io 
+var io = io.listen(server)
+  , buffer = [];
+  
+io.on('connection', function(client){
+  client.send({ buffer: buffer });
+  client.broadcast({ announcement: client.sessionId + ' connected' });
+  
+  client.on('message', function(message){
+    var msg = { message: [client.sessionId, message] };
+    buffer.push(msg);
+    if (buffer.length > 15) buffer.shift();
+    client.broadcast(msg);
+  });
+
+  client.on('disconnect', function(){
+    client.broadcast({ announcement: client.sessionId + ' disconnected' });
+  });
+});
 
 console.log('Server running at http://127.0.0.1:666/');
