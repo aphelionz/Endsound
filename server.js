10a151d6dc1ce2c0a5ca2d5abf0bcb957957bd9b
Mark Robert Henderson <henderson.mark@gmail.com>
2011-01-30 12:41:43 -0500

    File server update

--- server.js
+++ server.js
@@ -6,31 +6,25 @@ var http = require('http'),
     
 server = http.createServer(function(req, res){
   var path = url.parse(req.url).pathname;
+  if(path === "/") path = "/index.html";
+  var mime = {}
   
-  switch (path){
-    case '/':
-      path = "/index.html"
-      fs.readFile(__dirname + path, function(err, data){
-        if (err) return send404(res);
-        res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'})
-        res.write(data, 'utf8');
-        res.end();
-      });
-      break;
+  fs.readFile(__dirname + path, function(err, data){
+    if (err) return send404(res);
+    switch (getExtension(path)){
+      case 'html':  mime = {'Content-Type': 'text/html'};       break;
+      case "jpg":   mime = {'Content-Type': 'image/jpeg'};      break;
+      case "png":   mime = {'Content-Type': 'image/png'};       break;
+      case "gif":   mime = {'Content-Type': 'image/gif'};       break;
+      case "css":   mime = {'Content-Type': 'text/css'};        break;  
+      case "js":    mime = {'Content-Type': 'text/javascript'}; break;
+      default:      send404(res);                               break;
+    }
     
-    case "/lib/keymap.js":
-    case "/lib/audio.js":
-    case "/lib/vendor/jquery-1.4.4.js":
-      fs.readFile(__dirname + path, function(err, data){
-        if (err) return send404(res);
-        res.writeHead(200, {'Content-Type': 'text/javascript'})
-        res.write(data, 'utf8');
-        res.end();
-      });
-      break;
-
-    default: send404(res);
-  }
+    res.writeHead(200, {'Content-Type': mime})
+    res.write(data, 'utf8');
+    res.end();
+  });
 }),
 
 send404 = function(res){
@@ -62,3 +56,7 @@ io.on('connection', function(client){
 });
 
 console.log('Server running at http://127.0.0.1:666/');
+
+function getExtension(path) {
+  return (/[.]/.exec(path)) ? /[^.]+$/.exec(path)[0] : undefined;
+}
